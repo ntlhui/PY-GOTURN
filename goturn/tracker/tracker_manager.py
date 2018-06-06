ffbbc1ee683bed5b3c13ffba8948f04c2f6669e8
Nathan Hui <ntlhui@gmail.com>
2018-06-06 03:52:05 -0700

    Added timing and auto movie compilation

--- goturn/tracker/tracker_manager.py
+++ goturn/tracker/tracker_manager.py
@@ -4,10 +4,14 @@
 # Description: tracker manager
 
 import cv2 
+import os
+import time
+import subprocess
 
 opencv_version = cv2.__version__.split('.')[0]
 
 class tracker_manager:
+
 
     """Docstring for tracker_manager. """
 
@@ -30,6 +34,7 @@ class tracker_manager:
     def trackAll(self, start_video_num, pause_val):
         """Track the objects in the video
         """
+        output_path = "/datasets/home/64/564/nthui/output/"
 
         videos = self.videos
         objRegressor = self.regressor
@@ -37,9 +42,9 @@ class tracker_manager:
         logger = self.logger
 
         video_keys = list(videos.keys())
-        for i in range(start_video_num, len(videos)):
-            video_frames = videos[video_keys[i]][0]
-            annot_frames = videos[video_keys[i]][1]
+        for video_number in range(start_video_num, len(videos)):
+            video_frames = videos[video_keys[video_number]][0]
+            annot_frames = videos[video_keys[video_number]][1]
 
             num_frames = min(len(video_frames), len(annot_frames))
 
@@ -48,6 +53,10 @@ class tracker_manager:
             bbox_0 = annot_frames[0]
             sMatImage = cv2.imread(frame_0)
             objTracker.init(sMatImage, bbox_0, objRegressor)
+            video_output_dir = os.path.join(output_path, video_keys[video_number])
+            if not os.path.exists(video_output_dir):
+                os.mkdir(video_output_dir)
+            total_time = 0
             for i in range(1, num_frames):
                 frame = video_frames[i]
                 sMatImage = cv2.imread(frame)
@@ -58,13 +67,25 @@ class tracker_manager:
                     cv2.rectangle(sMatImageDraw, (int(bbox.x1), int(bbox.y1)), (int(bbox.x2), int(bbox.y2)), (255, 255, 255), 2)
                 else:
                     sMatImageDraw = cv2.rectangle(sMatImageDraw, (int(bbox.x1), int(bbox.y1)), (int(bbox.x2), int(bbox.y2)), (255, 255, 255), 2)
-
+                start = time.time()
                 bbox = objTracker.track(sMatImage, objRegressor)
+                end = time.time()
+                total_time += end - start
+
                 if opencv_version == '2':
                     cv2.rectangle(sMatImageDraw, (int(bbox.x1), int(bbox.y1)), (int(bbox.x2), int(bbox.y2)), (255, 0, 0), 2)
                 else:
                     sMatImageDraw = cv2.rectangle(sMatImageDraw, (int(bbox.x1), int(bbox.y1)), (int(bbox.x2), int(bbox.y2)), (255, 0, 0), 2)
 
                 # cv2.imshow('Results', sMatImageDraw)
-                cv2.imwrite("../output/results%04d.jpg" % (i), sMatImageDraw)
+                img_path = os.path.join(video_output_dir, "%04d.jpg" % (i))
+                self.logger.debug("Writing %s" % (img_path))
+                cv2.imwrite(img_path, sMatImageDraw)
                 # cv2.waitKey(10)
+            results_data_path = os.path.join(output_path, video_keys[video_number], "result.txt")
+            results_file = open(results_data_path, "w")
+            results_file.write(str(total_time / (num_frames - 1)))
+            results_file.close()
+            command = "ffmpeg -framerate 30 -y -i %s %s" % (os.path.join(video_output_dir, "%04d.jpg"), os.path.join(video_output_dir, "%s.mp4" % video_keys[video_number]))
+            subprocess.call(command, shell = True)
+
